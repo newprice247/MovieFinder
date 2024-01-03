d28a0b6aed767410f6d9d493d1a2434fe039f4fc
newprice247 <newprice247@gmail.com>
2024-01-03 15:44:20 -0500

    add darkmode to cards

--- client/src/components/modules/MovieCard.jsx
+++ client/src/components/modules/MovieCard.jsx
@@ -8,9 +8,9 @@ import {
  
 export default function SimpleCard(props) {
   return (
-    <Card className="mt-6 w-48 items-center justify-between flex flex-col">
+    <Card className="mt-6 w-48 items-center justify-between flex flex-col dark:bg-gray-800 dark:border-gray-600 dark:text-white bg-white border border-gray-300 rounded-lg shadow-md hover:shadow-lg transition duration-200 ease-in-out">
       <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2 sm:text-lg lg:text-sm font-semibold">
+        <Typography variant="h5" className="mb-2 sm:text-lg lg:text-sm font-semibold">
           {props.name}
         </Typography>
         <img 
